39cc5aa97de80754835f6882af6757221b11d474
sungjinchoi <csj7480@gmail.com>
2021-03-12 16:55:56 +0900

    Update Files for Web2 class

--- index.py
+++ index.py
@@ -1,16 +1,26 @@
 #!python
 print("content-type: text/html; charset=utf-8\n")
 
-import cgi
+import cgi,os,view
+
 form= cgi.FieldStorage()
 
 if 'id' in form:
     pageId = form["id"].value
     description = open('data/'+pageId , 'r').read()
+    update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
+    delete_action = '''
+        <form action = "process_delete.py" method = "post">
+            <input type="hidden" name="pageId" value="{}">
+            <input type="submit" value = "delete">
+        </form>
+    '''.format(pageId)
 
 else:
     pageId = 'Welcome'
     description = 'Hello web'
+    update_link=''
+    delete_action=''
 print('''<!doctype html>
 <html>
 <head>
@@ -20,13 +30,15 @@ print('''<!doctype html>
 <body>
   <h1><a href="index.py">WEB</a></h1>
   <ol>
-    <li><a href="index.py?id=HTML">HTML</a></li>
-    <li><a href="index.py?id=CSS">CSS</a></li>
-    <li><a href="index.py?id=JavaScript">JavaScript</a></li>
-    <li><a href="index.py?id=Python">Python</a></li>
+    {listStr}
   </ol>
+  <a href = "create.py">create</a>
+  {update_link}
+  {delete_action}
   <h2>{title}</h2>
   <p>{desc}</p>
 </body>
 </html>
-'''.format(title = pageId, desc = description))
+'''.format(title = pageId, desc = description,
+listStr = view.getList(),update_link = update_link,
+ delete_action=delete_action))
