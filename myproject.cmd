175e71dd1c600301024b44d1054fd90842ea72a9
sungjinchoi <csj7480@gmail.com>
2021-03-12 23:01:03 +0900

    Create myproject folder for Flask

---
+++ myproject.cmd
@@ -0,0 +1,7 @@
+@echo off
+cd c:/projects/myproject
+# ------------------------- [edit] ------------------------ #
+set FLASK_APP=pybo
+set FLASK_ENV=development
+# --------------------------------------------------------- #
+c:/venvs/myproject/scripts/activate
