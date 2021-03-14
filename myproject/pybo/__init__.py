7049ab26e738d0655619c4f5e69359eb35291f7c
sungjinchoi <csj7480@gmail.com>
2021-03-14 20:34:14 +0900

    Finish 3-03

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -16,11 +16,16 @@ def create_app():
     migrate.init_app(app,db)
 
     from . import models
-    
+
     # Blueprint
     from .views import main_views,question_views, answer_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
 
+
+    # 필터
+    from .filter import format_datetime
+    app.jinja_env.filters['datetime'] = format_datetime
+    
     return app
